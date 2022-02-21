040bcbec1349b39dc9167f9c6a3aa4f55d6fda0b
Ramil Mirhasanov <ramil600@yahoo.com>
2022-02-21 10:20:49 +0300

    changed tree structure of a project

--- app/user/server.go
+++ app/user/server.go
@@ -9,7 +9,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/ramil600/sensors2/business/web"
+	"github.com/ramil600/sensors2/foundation/web"
 )
 
 const (
--- foundation/web/web.go
+++ foundation/web/web.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/ramil600/sensors2/app/user/handlers"
+
 	"github.com/ramil600/sensors2/business/mid"
 )
 
